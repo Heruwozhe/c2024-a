a2c4177d137cc4f29c86687d7bf845c8b87a9df5
Heeuwozhe <1873498275@qq.com>
2024-09-27 16:09:26 +0800

    level1 finished.

--- level1/p01_running_letter/main.c
+++ level1/p01_running_letter/main.c
@@ -1,6 +1,31 @@
-#include <printf.h>
-
-int main() {
-    printf("hello world!\n");
+#include <stdio.h>
+#include <windows.h>
+#include <string.h>
+int main(void)
+{
+    char arr1[]="a                                               ";
+    for(int i = 0;i<strlen(arr1)-1;i++)
+    {
+        printf("%s",arr1);
+        arr1[i]=' ';
+        arr1[i+1]='a';
+        Sleep(200);
+        system("cls");
+    }
+    printf("%s",arr1);
+    system("cls");
+    Sleep(200);
+    for(int i = strlen(arr1)-1;i>0;i--)
+    {
+        printf("%s",arr1);
+        arr1[i]=' ';
+        arr1[i-1]='a';
+        Sleep(200);
+        system("cls");
+    }
+    printf("%s",arr1);
+    system("cls");
+    printf("Press enter to exit...");
+    getchar();
     return 0;
 }
