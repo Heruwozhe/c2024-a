ee059fec7dd7afb3f63d88ffec800e7e58e916a5
Heeuwozhe <1873498275@qq.com>
2024-09-27 17:31:03 +0800

    level1_p02 finished.

--- level1/p02_is_prime/main.c
+++ level1/p02_is_prime/main.c
@@ -1,6 +1,17 @@
-#include <printf.h>
-
-int main() {
-    printf("hello world!\n");
+#include <stdio.h>
+#include <math.h>
+int main(void){
+    int num = 0;int i = 2;
+    printf("Please enter a integer:");
+    scanf_s("%d", &num);
+    int dividing_number =sqrt(num);
+    for(;i<dividing_number+1;i++) {
+        if(num%i==0) {
+            printf("The number is not prime.");
+            break;
+        }
+    }
+    if(i==dividing_number+1)
+        printf("The number is prime.");
     return 0;
 }
