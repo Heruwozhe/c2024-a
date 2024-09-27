0e2a3961a9619a6be672d15a7609fa2554b1f0bd
Heeuwozhe <1873498275@qq.com>
2024-09-28 04:12:46 +0800

    level1_p03 finished.

--- level1/p03_all_primes/main.c
+++ level1/p03_all_primes/main.c
@@ -1,6 +1,27 @@
-#include <printf.h>
-
-int main() {
-    printf("hello world!\n");
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+int if_prime(int num) {
+    int i = 2;
+    int dividing_number =(int)sqrt(num);
+    for(;i<dividing_number+1;i++) {
+        if(num%i==0) {
+            return 0;
+        }
+    }
+    if(i==dividing_number+1)
+        return 1;
+}
+int main(void) {
+    clock_t start_time=clock();
+    int i = 3;
+    for(;i<998;i++) {
+        if(if_prime(i)==1)
+            printf("%d\t",i);
+    }
+    clock_t end_time=clock();
+    putchar('\n');
+    printf("The running time is %.4f seconds",(end_time-start_time)/1000.0);
     return 0;
 }
+//差不多这样了，再如何优化确实想不到了
